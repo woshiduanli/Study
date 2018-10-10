1ab40d52b35c5186aa82e0806f9c6b06e614f456
duanli <duanli@zwwxgame.com>
2018-10-10 14:40:47 +0800

    lua高效书写方法

---
+++ MyStudy/Lua/高效的lua代码书写方法.cs
@@ -0,0 +1,12 @@
+table 
+	用table的时候， 如果知道长度， 就初始化大小
+	table中数组和字典更省内存
+
+string
+	字符串.. 的方法， 速度比string.format更快， string.format主要是可读性好一些
+
+	变量， 能用local， 尽量都用local,全局变量的速度， 小于local 
+闭包
+	能用闭包调用函数，用闭包， 闭包效率更高
+
+
