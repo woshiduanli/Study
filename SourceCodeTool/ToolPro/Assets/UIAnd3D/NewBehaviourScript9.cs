1c657df08dae822a516637c6dfccbee9e1ef6f7e
duanli <773633450@qq.com>
2019-10-26 21:02:20 +0800

    修改时间空间位置

--- SourceCodeTool/ToolPro/Assets/UIAnd3D/NewBehaviourScript9.cs
+++ SourceCodeTool/ToolPro/Assets/UIAnd3D/NewBehaviourScript9.cs
@@ -8,9 +8,32 @@ public class NewBehaviourScript9 : MonoBehaviour {
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	private Vector3 m_screenPos;
+	/// <summary>
+	/// WolrdPostionToRectTransfromToWorldPos(m_Target.position, m_Trans, UI_Camera222.Instance.camera);
+	/// </summary>
+	/// <param name="worldPos"> ui要跟着的世界坐标空间  </param>
+	/// <param name="rect"> ui 对象 </param>
+	/// <param name="uiCamera">ui 相机 </param>
+	public void WolrdPostionToRectTransfromToWorldPos(Vector3 worldPos, RectTransform rect, Camera uiCamera)
+	{
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(worldPos);
+		if (m_screenPos == screenPos) return;
+		m_screenPos = screenPos;
+		rect.gameObject.transform.position = uiCamera.ScreenToWorldPoint(screenPos);
+		Vector3 localPos = rect.gameObject.transform.localPosition;
+		localPos.z = 0;
+		rect.gameObject.transform.localPosition = localPos;
 	}
+
+
+	void Update()
+	{
+//
+
+	}
+
+	// Update is called once per frame
+
 }
