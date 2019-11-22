1a755f5b146e4bd31f06efd1b5e7c840f36da8bc
duanli168 <769135040@qq.com>
2019-11-22 17:09:13 +0800

    bat

---
+++ SourceCodeTool/ToolPro/Assets/CommonBat/bocheng_copy.bat
@@ -0,0 +1,7 @@
+
+rd /s /q F:\ProjectqiPai\bocheng_client\bl_app_client\client\Assets\GameMain\Games\DZMJ
+
+md F:\ProjectqiPai\bocheng_client\bl_app_client\client\Assets\GameMain\Games\DZMJ
+
+
+XCOPY F:\ProjectqiPai\qipai5\bl_app_client\client\Assets\GameMain\Games\DZMJ\* F:\ProjectqiPai\bocheng_client\bl_app_client\client\Assets\GameMain\Games\DZMJ /s /e
